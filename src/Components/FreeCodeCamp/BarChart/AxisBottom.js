49656a9c1e8ae9cf9a84ac732ec08ca1e9d75ce5
DerekLeungHoWang <hkz88i00123@gmail.com>
2021-06-20 13:27:25 +0800

    first commit

---
+++ src/Components/FreeCodeCamp/BarChart/AxisBottom.js
@@ -0,0 +1,14 @@
+
+import React, { useEffect, useState } from 'react';
+ 
+import * as d3 from 'd3';
+
+export const AxisBottom = ({ xScale, innerHeight, tickFormat }) =>
+  xScale.ticks().map(tickValue => (
+    <g className="tick" key={tickValue} transform={`translate(${xScale(tickValue)},0)`}>
+      <line y2={innerHeight} />
+      <text style={{ textAnchor: 'middle' }} dy=".71em" y={innerHeight + 3}>
+        {tickFormat(tickValue)}
+      </text>
+    </g>
+  ));
---
+++ src/Components/FreeCodeCamp/BarChart/Marks.js
@@ -0,0 +1,27 @@
+
+import React, { useEffect, useState } from 'react';
+ 
+import * as d3 from 'd3';
+
+
+export const Marks = ({
+    data,
+    xScale,
+    yScale,
+    xValue,
+    yValue,
+    tooltipFormat
+  }) =>
+    data.map(d => (
+      <rect
+        className="mark"
+        key={yValue(d)}
+        x={0}
+        y={yScale(yValue(d))}
+        width={xScale(xValue(d))}
+        height={yScale.bandwidth()}
+      >
+        <title>{tooltipFormat(xValue(d))}</title>
+      </rect>
+    ));
+  
